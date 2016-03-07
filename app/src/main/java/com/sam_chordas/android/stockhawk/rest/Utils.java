944abad092b02a391fb2b18509824572b0a2ad00
Justin Mae <justin511@gmail.com>
2016-03-07 12:43:18 -0500

    Initial commit

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/rest/Utils.java
@@ -0,0 +1,104 @@
+package com.sam_chordas.android.stockhawk.rest;
+
+import android.content.ContentProviderOperation;
+import android.util.Log;
+
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by sam_chordas on 10/8/15.
+ */
+public class Utils {
+
+  private static String LOG_TAG = Utils.class.getSimpleName();
+
+  public static boolean showPercent = true;
+
+  public static ArrayList quoteJsonToContentVals(String JSON){
+    ArrayList<ContentProviderOperation> batchOperations = new ArrayList<>();
+    JSONObject jsonObject = null;
+    JSONArray resultsArray = null;
+    Log.i(LOG_TAG, "GET FB: " +JSON);
+    try{
+      jsonObject = new JSONObject(JSON);
+      if (jsonObject != null && jsonObject.length() != 0){
+        jsonObject = jsonObject.getJSONObject("query");
+        int count = Integer.parseInt(jsonObject.getString("count"));
+        // if there is only 1 result, data is stored under quote
+        if (count == 1){
+          jsonObject = jsonObject.getJSONObject("results")
+              .getJSONObject("quote");
+          batchOperations.add(buildBatchOperation(jsonObject));//
+        // if there is more than 1 result, data is stored under int within quote
+        } else{
+          resultsArray = jsonObject.getJSONObject("results").getJSONArray("quote");
+
+          if (resultsArray != null && resultsArray.length() != 0){
+            for (int i = 0; i < resultsArray.length(); i++){
+              jsonObject = resultsArray.getJSONObject(i);
+              batchOperations.add(buildBatchOperation(jsonObject));
+            }
+          }
+        }
+      }
+    } catch (JSONException e){
+      Log.e(LOG_TAG, "String to JSON failed: " + e);
+    }
+    return batchOperations;
+  }
+
+  // two digits after decimal point
+  public static String truncateBidPrice(String bidPrice){
+    bidPrice = String.format("%.2f", Float.parseFloat(bidPrice));
+    return bidPrice;
+  }
+
+  public static String truncateChange(String change, boolean isPercentChange){
+    // substring: start inclusive, end exclusive
+    String sign = change.substring(0,1);  // + or -
+    String percentSign = "";  // % - percent sign
+    if (isPercentChange){
+      percentSign = change.substring(change.length() - 1, change.length());
+      change = change.substring(0, change.length() - 1);
+    }
+    change = change.substring(1, change.length());  // getting just the numbers (without sign or %)
+    double round = (double) Math.round(Double.parseDouble(change) * 100) / 100;   // 2.98 --> 298.0 --> round --> 2.98
+    change = String.format("%.2f", round);
+    StringBuffer changeBuffer = new StringBuffer(change); // 2.98
+    changeBuffer.insert(0, sign); // +2.98
+    changeBuffer.append(percentSign); // +2.98% or +2.98 (if isPercentChange = false)
+    change = changeBuffer.toString();
+    return change;
+  }
+
+  public static ContentProviderOperation buildBatchOperation(JSONObject jsonObject){
+    // content://com.sam_chordas.android.stockhawk.data.QuoteProvider.quotes
+    ContentProviderOperation.Builder builder = ContentProviderOperation.newInsert(
+        QuoteProvider.Quotes.CONTENT_URI);
+    try {
+      String change = jsonObject.getString("Change");
+      builder.withValue(QuoteColumns.SYMBOL, jsonObject.getString("symbol"));
+      builder.withValue(QuoteColumns.BIDPRICE, truncateBidPrice(jsonObject.getString("Bid")));
+      builder.withValue(QuoteColumns.PERCENT_CHANGE, truncateChange(
+          jsonObject.getString("ChangeinPercent"), true));
+      builder.withValue(QuoteColumns.CHANGE, truncateChange(change, false));
+      builder.withValue(QuoteColumns.ISCURRENT, 1);
+      if (change.charAt(0) == '-'){
+        builder.withValue(QuoteColumns.ISUP, 0);
+      }else{
+        builder.withValue(QuoteColumns.ISUP, 1);
+      }
+
+    } catch (JSONException e){
+      e.printStackTrace();
+    }
+    return builder.build();
+  }
+}
